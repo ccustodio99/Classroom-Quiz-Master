1f83eb9150a841664e21ff2ec50994f104996903
CHRISTIAN CUSTODIO <c.custodio@lcc-daet.edu.ph>
2025-10-16 15:27:53 +0800

    Remove bundled Gradle wrapper binaries

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,3 @@
 plugins {
-    id("com.android.application") version "8.2.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
+    kotlin("jvm") version "1.9.23" apply false
 }
