9b99351470f017a54e085788093c4124602b25b6
CHRISTIAN CUSTODIO <c.custodio@lcc-daet.edu.ph>
2025-10-16 14:42:07 +0800

    Remove Gradle wrapper jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,9 @@
 plugins {
-    id("com.android.application") version "8.2.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
+    kotlin("jvm") version "1.9.23" apply false
+}
+
+allprojects {
+    repositories {
+        mavenCentral()
+    }
 }
